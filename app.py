d3fc5abfd1ae775b99a80f19b9525ea14aa6a0b4
demidraksin <greatraksin@icloud.com>
2021-01-15 11:28:07 +0300

    Initial commit. File main file and configuration.

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask  # импорт библиотеки
+
+app = Flask(__name__, instance_relative_config=False)
+# создаем переменную сайта, отключаем автоматический сбор конфигурации
+app.config.from_object('config.Config')  # указываем, где брать настройки сайта
+
+if __name__ == '__main__':  # инструкция запуска сайта
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# указываем корневую папку проекта
+basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = '/Users/demidraksin/PycharmProjects/8B_Flask_Lessons/app.py'
+# basedir = взять_абсолютный_путь(директория(главный_файл_сайта))
