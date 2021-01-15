d7cfd110c94d0aa5a8910541a280dce47cc2718b
demidraksin <greatraksin@icloud.com>
2021-01-15 12:10:12 +0300

    Finishing part1. Basic routing

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__, instance_relative_config=False)
 # создаем переменную сайта, отключаем автоматический сбор конфигурации
 app.config.from_object('config.Config')  # указываем, где брать настройки сайта
 
+from routes import *
+
 if __name__ == '__main__':  # инструкция запуска сайта
     app.run(debug=True)
